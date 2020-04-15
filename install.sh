75352bbc07a87c3bb0fc80e6073bf33c3d9a3bbe
Juan Daniel Polanco Avalos <=>
2020-04-15 16:47:56 -0500

    [Feat] improve install bash script configuration

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+PIO_PATH =/home/polanco/.platformio/penv/bin/pio
+
 echo "Instalando dependencias"
 sudo apt install make build-essential python python-pip gcc-avr avr-libc avrdude
 echo "Instalando dependencias de python"
 sudo pip install pySerial numpy matplotlib argparse
-echo "Includendo platformio al PATH"
-sudo ls -s /home/leonelmora/.platformio/penv/bin/pio /usr/bin/
 echo "Creando carpetas necesarias"
 sudo mkdir ./obj/
 sudo mkdir ./bin/
+echo "Includendo platformio al PATH"
+sudo ls -s $PIO_PATH /usr/bin/
