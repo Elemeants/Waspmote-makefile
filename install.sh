ac66f416e804bdec397de161a1710c5eb6d5170f
Daniel Polanco <jdanypa@gmail.com>
2020-04-16 00:47:18 +0000

    [Feat] Install: add python csv dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 PIO_PATH =/home/polanco/.platformio/penv/bin/pio
 
 echo "Instalando dependencias"
-sudo apt install make build-essential python python-pip gcc-avr avr-libc avrdude
+sudo apt install make build-essential python python-pip gcc-avr avr-libc avrdude python-tk python3-tk
 echo "Instalando dependencias de python"
 sudo pip install pySerial numpy matplotlib argparse
 echo "Creando carpetas necesarias"
